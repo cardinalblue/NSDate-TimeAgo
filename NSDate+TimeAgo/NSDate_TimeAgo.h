f817a86b31f3e1436a6140286102270d53a92316
yyjim <yy.jim731@gmail.com>
2019-05-31 12:18:41 +0800

    Init

---
+++ NSDate+TimeAgo/NSDate_TimeAgo.h
@@ -0,0 +1,19 @@
+//
+//  NSDate_TimeAgo.h
+//  NSDate+TimeAgo
+//
+//  Created by yyjim on 2019/5/31.
+//  Copyright © 2019 Cardinalblue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NSDate_TimeAgo.
+FOUNDATION_EXPORT double NSDate_TimeAgoVersionNumber;
+
+//! Project version string for NSDate_TimeAgo.
+FOUNDATION_EXPORT const unsigned char NSDate_TimeAgoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSDate_TimeAgo/PublicHeader.h>
+
+#import "NSDate+TimeAgo.h"
